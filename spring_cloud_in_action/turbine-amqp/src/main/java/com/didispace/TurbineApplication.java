eac514d29a6f23b635c3ac693696022701881430
翟永超 <dyc87112@qq.com>
2016-10-23 15:29:42 +0800

    《Spring Cloud实战》更新

--- spring_cloud_in_action/turbine-amqp/src/main/java/com/didispace/TurbineApplication.java
+++ spring_cloud_in_action/turbine-amqp/src/main/java/com/didispace/TurbineApplication.java
@@ -3,12 +3,12 @@ package com.didispace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableTurbine
+@EnableTurbineStream
 @EnableDiscoveryClient
 public class TurbineApplication {
 
