c65f67dc8b153423542324870d19db6ce569a1dd
翟永超 <dyc87112@qq.com>
2016-11-12 21:37:19 +0800

    《Spring Cloud实战》更新

--- spring_cloud_in_action/stream-hello/src/test/java/com/didispace/HelloApplicationTests.java
+++ spring_cloud_in_action/stream-hello/src/test/java/com/didispace/HelloApplicationTests.java
@@ -3,9 +3,11 @@ package com.didispace;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -13,15 +15,19 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = HelloApplication.class)
 @WebAppConfiguration
-@DirtiesContext
+//@DirtiesContext
 public class HelloApplicationTests {
 
 	@Autowired
 	private SinkOut sinkOut;
 
+	@Autowired @Qualifier("input")
+	private MessageChannel output;
+
 	@Test
 	public void contextLoads() {
-		sinkOut.output().send(MessageBuilder.withPayload("didi").build());
+//		sinkOut.output().send(MessageBuilder.withPayload("From sinkOut").build());
+		output.send(MessageBuilder.withPayload("From MessageChannel").build());
 	}
 
 }
