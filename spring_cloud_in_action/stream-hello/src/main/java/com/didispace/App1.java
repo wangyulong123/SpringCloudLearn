4d2961a97a7994d2306473bc6743189428068016
翟永超 <dyc87112@qq.com>
2016-11-16 20:53:41 +0800

    《Spring Cloud实战》更新

--- spring_cloud_in_action/stream-hello/src/main/java/com/didispace/App1.java
+++ spring_cloud_in_action/stream-hello/src/main/java/com/didispace/App1.java
@@ -5,10 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Processor;
-import org.springframework.cloud.stream.messaging.Sink;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  *
@@ -17,24 +14,23 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @create 2016/11/8.
  * @blog http://blog.didispace.com
  */
-@EnableScheduling
-@EnableBinding(value = {Processor.class})
+//@EnableBinding(value = {Processor.class})
 public class App1 {
 
     private static Logger logger = LoggerFactory.getLogger(HelloApplication.class);
 
-//    @StreamListener(Processor.INPUT)
-//    @SendTo(Processor.OUTPUT)
-//    public Object receiveFromInput(Object payload) {
-//        logger.info("Received: " + payload);
-//        return "From Input Channel Return - " + payload;
-//    }
-
-
-    @ServiceActivator(inputChannel= Processor.INPUT, outputChannel = Processor.OUTPUT)
+    @StreamListener(Processor.INPUT)
+    @SendTo(Processor.OUTPUT)
     public Object receiveFromInput(Object payload) {
         logger.info("Received: " + payload);
         return "From Input Channel Return - " + payload;
     }
+
+
+//    @ServiceActivator(inputChannel= Processor.INPUT, outputChannel = Processor.OUTPUT)
+//    public Object receiveFromInput(Object payload) {
+//        logger.info("Received: " + payload);
+//        return "From Input Channel Return - " + payload;
+//    }
 
 }
