0b7eff51e0bc576f9d207a37738b22b48cd0b09e
翟永超 <dyc87112@qq.com>
2016-11-23 09:21:12 +0800

    《Spring Cloud实战》更新

--- spring_cloud_in_action/trace-2/src/main/java/com/didispace/TraceApplication.java
+++ spring_cloud_in_action/trace-2/src/main/java/com/didispace/TraceApplication.java
@@ -1,6 +1,7 @@
 package com.didispace;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,16 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TraceApplication {
 
-	private final Logger logger = Logger.getLogger(getClass());
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@RequestMapping(value = "/trace-2", method = RequestMethod.GET)
-	public String trace() {
-		logger.info("===<call trace-2>===");
+	public String trace(HttpServletRequest request) {
+		logger.info("===<call trace-2, TraceId={}, SpanId={}>===",
+				request.getHeader("X-B3-TraceId"), request.getHeader("X-B3-SpanId"));
 		return "Trace";
 	}
 
