1238e75b8e250099771ee9382bf8ecc34ae1fc07
翟永超 <dyc87112@qq.com>
2016-12-10 19:45:47 +0800

    《Spring Cloud实战》更新

--- spring_cloud_in_action/stream-consumer/src/test/java/com/didispace/HelloApplicationTests.java
+++ spring_cloud_in_action/stream-consumer/src/test/java/com/didispace/HelloApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -20,6 +22,15 @@ public class HelloApplicationTests {
 	@Test
 	public void contextLoads() {
 		sinkSender.output().send(MessageBuilder.withPayload("From SinkSender").build());
+	}
+
+	public interface SinkSender {
+
+		String OUTPUT = "input";
+
+		@Output(SinkSender.OUTPUT)
+		MessageChannel output();
+
 	}
 
 }
